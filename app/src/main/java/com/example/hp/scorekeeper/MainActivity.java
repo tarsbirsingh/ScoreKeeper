440b37350cb831ed7160ef51d61be507a0fe356f
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-06-16 17:34:41 +0530

    reset method add

--- app/src/main/java/com/example/hp/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/hp/scorekeeper/MainActivity.java
@@ -17,13 +17,30 @@ int score = 0;
         score+=1;
         displayForTeamA(score);
     }
-    public void addOneForTeamB(View v){
+    public void addTwoForTeamA(View v){
         score+=2;
         displayForTeamA(score);
+    }
+    /**Second Team*/
+    public void addOneForTeamB(View v){
+        score+=1;
+        displayForTeamB(score);
+    }
+    public void addTwoForTeamB(View v){
+        score+=2;
+        displayForTeamB(score);
     }
     public void displayForTeamA(int score){
         TextView scoreView  = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-
+public void displayForTeamB(int score){
+        TextView scoreViewB= findViewById(R.id.team_b_score);
+        scoreViewB.setText(String.valueOf(score));
+}
+public void resetScore(View v){
+        score=0;
+        displayForTeamA(score);
+        displayForTeamB(score);
+}
 }
